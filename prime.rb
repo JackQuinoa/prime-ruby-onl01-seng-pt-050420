20df53517a4391e1e1ea6af1c7e2d9434fdd5a04
Jack Quinoa <jquinoa208@gmail.com>
2020-05-15 20:14:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,9 @@
 # Add  code here!
 def prime?(x)
-  (2..x-1).to_a.all? { |integer| x % integer != 0 }
+  if x < 0 
+    return false
+  else
+    return (2..x-1).to_a.all? { |integer| x % integer != 0 }
  
 ## x % 1 == 0 && x % x  == 0 
 end
